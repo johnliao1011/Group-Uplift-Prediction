9ed77478bf18fec4417c1b146bd7fa8a74cde340
John Liao <john.liao@iss.nthu.edu.tw>
2019-05-02 07:56:36 +0800

    plot update

--- Plot.R
+++ Plot.R
@@ -1,5 +1,5 @@
 
-Custom_Plot <- function(data, yLim, LineType, LineColor, LineWidth,main =FALSE){
+Custom_Plot <- function(data, yLim, LineType, LineColor, Pch, Lty, TransAlpha, LineWidth,main =FALSE, Title){
   
   # summary of the group each bootstrap generate
   summary <- data%>% apply( 2, function(x)(length(x)-1-length(which(is.na(x)))))%>%
@@ -10,7 +10,7 @@ Custom_Plot <- function(data, yLim, LineType, LineColor, LineWidth,main =FALSE){
   # generate a blank plot
   if(main ==TRUE){
     plot(x = seq(0, 100, by = 10), y = seq(0, 1, by = 0.1), type="l", lwd=0,ylim = yLim,
-         ylab="Cumulative Incremental Gains", xlab="Proportion of population targeted (%)")
+         ylab="Cumulative Incremental Gains", xlab="Proportion of population targeted (%)", main =Title)
     
     lines(x=seq(0, 100, by = 10), y = seq(0, 1, by = 1/10), lwd=4, lty="dashed")
   }
@@ -20,17 +20,13 @@ Custom_Plot <- function(data, yLim, LineType, LineColor, LineWidth,main =FALSE){
   
   for (i in c(1:ncol(data))) {
     upliftValue<-data[, index[i]]%>%na.omit()
-    lines(x = seq(0, 100, by = 100/(length(upliftValue)-1)), y = upliftValue, type = LineType, pch = 19,
-          col = LineColor, lwd=LineWidth) 
+    rgbDetect <- col2rgb(LineColor)
+    lines(x = seq(0, 100, by = 100/(length(upliftValue)-1)), y = upliftValue, type = LineType, pch = Pch,
+          col = rgb(rgbDetect[1], rgbDetect[2], rgbDetect[3], maxColorValue = 255, alpha = TransAlpha), lwd=LineWidth,  lty=Lty) 
   }
 }
 
 
 
-Custom_Plot(data = bootstrap.1.1.1.1_tma.RF$gain.ratio, yLim = c(0,2), 
-            LineType = "b", LineColor = "yellowgreen", LineWidth = 2, main = TRUE)
-
-Custom_Plot(data = bootstrap.1.1.1.1_oma.RF$gain.ratio, yLim = c(0,2), 
-            LineType = "b", LineColor = "orangered", LineWidth = 2)
 
 
